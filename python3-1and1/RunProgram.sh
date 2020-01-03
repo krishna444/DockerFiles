1201c9fccce0e24d2a943f966bcf63954cc90340
Krishna Paudel <krishna.paudel@1und1.de>
2020-01-03 16:19:42 +0100

    Commands changed to show the python version

--- python3-1and1/RunProgram.sh
+++ python3-1and1/RunProgram.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 sudo docker build -t python3:1and1 .
 CONTAINER_PATH=/root/1and1/tariff-generator-bk
 IMAGE=python3:1and1
-COMMANDS="cd tariff-generator-bk && python TariffUtils.py"
-#COMMANDS="python -V"
+#COMMANDS="cd tariff-generator-bk && python TariffUtils.py"
+COMMANDS="python -V"
 sudo apt update && sudo apt install -y docker
 sudo docker run -it --rm --name tariff-generator  -v "$PWD":$CONTAINER_PATH $IMAGE /bin/bash -c "$COMMANDS"
