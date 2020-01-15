f99becdc948ab9f9c22c3af731aff80d0286b993
krishna <krishna444@gmail.com>
2020-01-16 00:22:23 +0100

    Restart implmeneted for portainer, shell script renamed

--- portainer/RunContainer.sh
+++ portainer/RunContainer.sh
@@ -4,6 +4,8 @@
 # @Author: Krishna Paudel              #
 # @Date: 2020.01.15                    #
 ########################################
+sudo docker stop portainer
+sudo docker rm portainer || sleep 10
 sudo docker volume create portainer_data
-sudo docker run -d --rm -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer portainer/portainer
+sudo docker run -itd --restart=always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer portainer/portainer
 sudo docker logs --follow portainer
