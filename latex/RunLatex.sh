05d9a96ff158a67907169dec77c231d50f3ded1d
Krishna Paudel <krishna.paudel@1und1.de>
2020-01-20 17:36:39 +0100

    Script changed

--- latex/RunLatex.sh
+++ latex/RunLatex.sh
@@ -2,4 +2,4 @@
 IMAGE=krishna444/latex:debian
 #sudo docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data $IMAGE $@
 #sudo docker run --rm -i -v "$PWD":/source "$IMAGE" "$@"
-sudo docker run --rm -it --net=none -v $PWD:/source  krishna444/latex:debian /bin/bash -c "cd workspace;pdflatex test.tex"
+sudo docker run --rm -it --net=none -v $PWD:/source  krishna444/latex:debian /bin/bash -c "cd workspace;pdflatex beamer-it.tex"
