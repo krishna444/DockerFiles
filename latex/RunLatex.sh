d3239cb5b48a56dd66ac3c4588a4c6679222106c
Krishna Paudel <krishna.paudel@1und1.de>
2020-01-27 12:12:44 +0100

    Tex file renamed, cached files remvoed form VSCode tracking

--- latex/RunLatex.sh
+++ latex/RunLatex.sh
@@ -2,4 +2,5 @@
 IMAGE=krishna444/latex:debian
 #sudo docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data $IMAGE $@
 #sudo docker run --rm -i -v "$PWD":/source "$IMAGE" "$@"
-sudo docker run --rm -it --net=none -v $PWD:/source  krishna444/latex:debian /bin/bash -c "cd workspace;pdflatex beamer-it.tex"
+MAIN_FILE="TariffGenerator.tex"
+sudo docker run --rm -it --net=none -v $PWD:/source  krishna444/latex:debian /bin/bash -c "cd workspace;pdflatex $MAIN_FILE"
