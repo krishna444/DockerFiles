2f4ff41bd5449e21cc7e207bcdb3354134ba1564
Krishna Paudel <krishna.paudel@1und1.de>
2020-01-27 14:58:45 +0100

    Pdf file included in the git repository. pdfview with okular removed

--- latex/RunLatex.sh
+++ latex/RunLatex.sh
@@ -4,3 +4,4 @@ IMAGE=krishna444/latex:debian
 #sudo docker run --rm -i -v "$PWD":/source "$IMAGE" "$@"
 MAIN_FILE="TariffGenerator.tex"
 sudo docker run --rm -it --net=none -v $PWD:/source  krishna444/latex:debian /bin/bash -c "cd workspace;pdflatex $MAIN_FILE"
+#okular workspace/$MAIN_FILE.pdf
